074ea6f7cdd4f724184a738f475677743e4e7c78
Lukasz <226426@student.pwr.wroc.pl>
2017-12-28 15:16:49 +0100

    all: Poczatek

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+class Konwerter
+{
+    int liczba_jednosci;
+    int liczba_dziesiatek;
+    int liczba_setek;
+    int liczba_tysiecy;
+    Konwerter(int podana_liczba)
+    {
+        liczba_jednosci = podana_liczba%10;
+        liczba_dziesiatek = podana_liczba/10%10;
+        liczba_setek = podana_liczba/100%10;
+        liczba_tysiecy = podana_liczba/1000;
+    }
+};
+
+int main()
+{
+    return 0;   
+}
