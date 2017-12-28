9cd1d37a4bf6ac56fa3e3e12e083c27910bc89b8
Lukasz <226426@student.pwr.wroc.pl>
2017-12-28 16:15:46 +0100

    Etap 3

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Konwerter
 {
+    public:
     int liczba_jednosci;
     int liczba_dziesiatek;
     int liczba_setek;
@@ -15,11 +16,14 @@ class Konwerter
         liczba_setek = podana_liczba/100%10;
         liczba_tysiecy = podana_liczba/1000;
     }
+    
 };
 
 int main()
 {
     int liczba;
     cin>>liczba;
+    Konwerter K(liczba);
+    cout << K.liczba_jednosci << endl << K.liczba_dziesiatek << endl << K.liczba_setek << endl << K.liczba_tysiecy << endl;
     return 0;   
 }
